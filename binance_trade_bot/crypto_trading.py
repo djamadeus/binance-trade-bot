7bbf72a537d0fa48827768f8c0eb61825c4b36fd
Stefan <stefan@wunderdata.com>
2021-06-02 03:07:50 +0200

    timer fix (stupid scheduler removed)

--- binance_trade_bot/crypto_trading.py
+++ binance_trade_bot/crypto_trading.py
@@ -39,9 +39,8 @@ def main():
 
     #trader.initialize()
     trader.update_values()
-    schedule = SafeScheduler(logger)
     # schedule.every(config.SCOUT_SLEEP_TIME).seconds.do(trader.scout).tag("scouting")
-    schedule.every(1).minutes.do(trader.update_values).tag("updating value history")
+    #schedule.every(1).minutes.do(trader.update_values).tag("updating value history")
     #schedule.every(1).minutes.do(trader.update_values_eur).tag("updating value history")
     # schedule.every(1).minutes.do(db.prune_scout_history).tag("pruning scout history")
     # schedule.every(1).hours.do(db.prune_value_history).tag("pruning value history")
@@ -49,8 +48,6 @@ def main():
     while True:
         if math.floor(time.time() / 60) > starting:
             starting = math.floor(time.time() / 60)
-            logger.info("starting" + str(starting))
-            schedule.run_pending()
+            trader.update_values()
         time.sleep(1)
-        logger.info(math.floor(time.time() / 60))
 
